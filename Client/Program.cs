06c23e2d05574adc5d8d8c8a79d82106525857ed
João Miguel Do Ó Matos Soares <ist189475@tecnico.ulisboa.pt>
2020-10-11 09:24:13 +0100

    Initial Commit: experimenting with gRPC using .NET

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Server;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            //HelloRequest input = new HelloRequest { Name = "Rox" };
+            //Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+            //HelloReply reply = await client.SayHelloAsync(input);
+            //Console.WriteLine(reply.Message);
+
+            Customer.CustomerClient client = new Customer.CustomerClient(channel);
+
+            CustomerLookupModel input = new CustomerLookupModel { UserId = 1 };
+            CustomerModel reply = await client.GetCustomerInfoAsync(input);
+            Console.WriteLine("Received customer: {0} {1}", reply.FirstName, reply.LastName);
+
+            using (AsyncServerStreamingCall<CustomerModel> call = client.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    CustomerModel customer = call.ResponseStream.Current;
+                    Console.WriteLine("New customer: {0} {1}", customer.FirstName, customer.LastName);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
